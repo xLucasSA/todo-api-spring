b992ef753584c3f0f752c9a03215056bd73bc2f2
Lucas <lsdeabre@gmail.com>
2024-07-05 09:23:24 -0300

    First commit

---
+++ src/main/java/com/lucasabreu/simpletodo/SimpletodoApplication.java
@@ -0,0 +1,13 @@
+package com.lucasabreu.simpletodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpletodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpletodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasabreu/simpletodo/SimpletodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasabreu.simpletodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpletodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
