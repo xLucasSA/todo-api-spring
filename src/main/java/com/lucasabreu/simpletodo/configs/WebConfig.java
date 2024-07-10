618c52f317fe47f0bb38011037af3eff65665595
Lucas <lsdeabre@gmail.com>
2024-07-10 00:38:49 -0300

    Create WebConfig

---
+++ src/main/java/com/lucasabreu/simpletodo/configs/WebConfig.java
@@ -0,0 +1,17 @@
+package com.lucasabreu.simpletodo.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
